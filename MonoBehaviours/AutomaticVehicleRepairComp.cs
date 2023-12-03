4561b8c1b26e3b8aa4e7958dfdf0c24eaa789f4e
MarsIandare <122297918+MarsIandare@users.noreply.github.com>
2023-12-03 18:02:58 +0100

    Add files via upload

---
+++ MonoBehaviours/AutomaticVehicleRepairComp.cs
@@ -0,0 +1,254 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nautilus.Extensions;
+using Nautilus.Utility;
+
+namespace AutomaticVehicleRepair.MonoBehaviours
+{
+    // TODO Review this file and update to your own requirements, or remove it altogether if not required
+    /// <summary>
+    /// Template MonoBehaviour class. Use this to add new functionality and behaviours to
+    /// the game.
+    /// </summary>
+    internal class AutomaticVehicleRepairComp : MonoBehaviour
+    {
+        public Vehicle vehicle;
+        public LiveMixin liveMixin;
+        public EnergyInterface energyInterface;
+        public CrushDamage crushDamage;
+        public FMODASRPlayer weldSound;
+        public SubRoot subRoot;
+
+        private float energyPerPercentage;
+        private float healthPerPercentage;
+        private float timer = 0;
+        private bool canShowNoPowerNotification = true;
+        private bool active = true;
+        private bool toggleKeyPressed = false;
+        private float creationTime;
+        private float lastDamage;
+        private float lastHealth;
+        private bool allowToggle = false;
+        private float lastRepair;
+        private bool isVanillaSub = false;
+
+        public void Start()
+        {
+            creationTime = Time.time;
+            liveMixin = vehicle.liveMixin;
+            energyInterface = vehicle.gameObject.GetComponent<EnergyInterface>();
+            crushDamage = vehicle.gameObject.GetComponent<CrushDamage>();
+            
+            int num = energyInterface.sources.Length;
+            float totalEnergy = 0f;
+
+            for (int i = 0; i < num; ++i)
+            {
+                if (energyInterface.sources[i] != null)
+                {
+                    totalEnergy += 200;
+                }
+            }
+
+            energyPerPercentage = totalEnergy / 100;
+            healthPerPercentage = liveMixin.maxHealth / 100;
+            
+            if (gameObject.name == "SeaMoth(Clone)" || gameObject.name == "Exosuit(Clone)")
+            {
+                isVanillaSub = true;
+            }
+
+            subRoot = gameObject.GetComponent<SubRoot>();
+
+            UWE.CoroutineHost.StartCoroutine(CheckForRoot(gameObject, this));
+            UWE.CoroutineHost.StartCoroutine(GetWelderSound(gameObject, this));
+        }
+        public void Update()
+        {
+            if (!isVanillaSub && !RepairPlugin.config.moddedVehicles)
+            {
+                ToggleSound(false);
+                return;
+            }
+
+            switch (RepairPlugin.config.repairType)
+            {
+                case RepairPlugin.RepairTypes.Passive:
+                {
+                        allowToggle = false;
+                        if (!active)
+                        {
+                            ErrorMessage.AddMessage("Automatic repairing now online.");
+                        }
+                        active = true; break;
+                }
+                case RepairPlugin.RepairTypes.Toggle:
+                {
+                        allowToggle = true;
+                        break;
+                }
+            }
+            if (GameInput.GetKeyDown(RepairPlugin.config.toggleKey) && !toggleKeyPressed && allowToggle)
+            {
+                if (EnoughPower() && Player.main.currentMountedVehicle == vehicle)
+                {
+                    active = !active;
+                    if (active)
+                    {
+                        ErrorMessage.AddMessage("Automatic repairing now online.");
+                    }
+                    else
+                    {
+                        ErrorMessage.AddMessage("Automatic repairing now offline.");
+                        ToggleSound(false);
+                    }
+                }
+                toggleKeyPressed = true;
+            }
+            if (GameInput.GetKeyUp(RepairPlugin.config.toggleKey) && toggleKeyPressed)
+            {
+                toggleKeyPressed = false;
+            }
+            if (timer - Time.time >= RepairPlugin.config.healInterval)
+            {
+                timer = Time.time + RepairPlugin.config.healInterval;
+                return;
+            }
+            if (timer <= Time.time)
+            {
+                timer = Time.time + RepairPlugin.config.healInterval;
+                if (liveMixin != null && active && energyInterface != null)
+                {
+                    if (EnoughPower())
+                    {
+                        if (liveMixin.health != liveMixin.maxHealth && !HasTakenRecentDamage())
+                        {
+                            if (BelowCrushDepth() && !RepairPlugin.config.crushDepth)
+                            {
+                                ToggleSound(false);
+                                return;
+                            }
+
+                            liveMixin.AddHealth(healthPerPercentage * RepairPlugin.config.healthPerHeal);
+                            energyInterface.ConsumeEnergy(energyPerPercentage * RepairPlugin.config.healCost);
+                            canShowNoPowerNotification = true;
+                            lastRepair = Time.time;
+                            if (weldSound != null && RepairPlugin.config.repairSound)
+                            {
+                                if (liveMixin.health != liveMixin.maxHealth)
+                                {
+                                    weldSound.Play();
+                                }
+                                else
+                                {
+                                    weldSound.Stop();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ToggleSound(false);
+                        }
+                    }
+                    else if (canShowNoPowerNotification && creationTime <= Time.time - 1)
+                    {
+                        ErrorMessage.AddMessage("Low energy levels detected, automatic reparing now offline.");
+                        canShowNoPowerNotification = false;
+                        ToggleSound(false);
+                    }
+                }
+                else
+                {
+                    ToggleSound(false);
+                }
+            }
+
+            if (Time.time >= lastRepair + 2f || !RepairPlugin.config.repairSound || Player.main.currentMountedVehicle != vehicle)
+            {
+                ToggleSound(false);
+            }
+
+            if (liveMixin != null)
+            {
+                if (liveMixin.health <= lastHealth && liveMixin.health != lastHealth)
+                {
+                    lastDamage = Time.time;
+                }
+                lastHealth = liveMixin.health;
+            }
+        }
+        
+        public float EnergyRemaining(EnergyMixin[] sources)
+        {
+            int num = sources.Length;
+            float energyPercentage = 0f;
+            for (int i = 0; i < num; ++i)
+            {
+                if (sources[i] != null)
+                {
+                    energyPercentage += sources[i].GetEnergyScalar() / sources.Length;
+                }
+            }
+            return energyPercentage;
+        }
+
+        private bool EnoughPower()
+        {
+            if (energyInterface != null)
+            {
+                if (EnergyRemaining(energyInterface.sources) >= RepairPlugin.config.energyRequirement / 100)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void ToggleSound(bool toggle)
+        {
+            if (weldSound != null)
+            {
+                if (toggle)
+                {
+                    weldSound.Play();
+                }
+                else
+                {
+                    weldSound.Stop();
+                }
+            }
+        }
+        private bool BelowCrushDepth()
+        {
+            return crushDamage.GetCanTakeCrushDamage() && crushDamage.GetDepth() > crushDamage.crushDepth;
+        }
+        private bool HasTakenRecentDamage()
+        {
+            return Time.time <= lastDamage + RepairPlugin.config.stunnedTime;
+        }
+        private static IEnumerator GetWelderSound(GameObject parent, AutomaticVehicleRepairComp repairComp)
+        {
+            CoroutineTask<GameObject> task = CraftData.GetPrefabForTechTypeAsync(TechType.Welder);
+            yield return task;
+            GameObject gameObjectPrefab = task.GetResult();
+            FMODASRPlayer orgWeldSound = gameObjectPrefab.GetComponent<Welder>().weldSound;
+
+            repairComp.weldSound = parent.AddComponent<FMODASRPlayer>();
+            FMOD_StudioEventEmitter startSound = parent.AddComponent<FMOD_StudioEventEmitter>();
+            startSound.CopyComponent(orgWeldSound.startLoopSound);
+
+            repairComp.weldSound.startLoopSound = startSound;
+        }
+
+        private static IEnumerator CheckForRoot(GameObject root, AutomaticVehicleRepairComp repairComp)
+        {
+            yield return new WaitForSeconds(2f);
+
+            SubRoot subRoot = root.GetComponent<SubRoot>();
+
+            if (subRoot != null && subRoot.damageManager != null)
+            {
+                repairComp.enabled = false;
+            }
+        }
+    }
+}
---
+++ Patches/HolographicHudPatches.cs
@@ -0,0 +1,83 @@
+﻿using AutomaticVehicleRepair.MonoBehaviours;
+using HarmonyLib;
+using UnityEngine.UI;
+using UnityEngine;
+using static CyclopsHolographicHUD;
+using System.Collections.Generic;
+
+namespace AutomaticVehicleRepair.Patches
+{
+    // TODO Review this file and update to your own requirements, or remove it altogether if not required
+
+    /// <summary>
+    /// Sample Harmony Patch class. Suggestion is to use one file per patched class
+    /// though you can include multiple patch classes in one file.
+    /// Below is included as an example, and should be replaced by classes and methods
+    /// for your mod.
+    /// </summary>
+    [HarmonyPatch(typeof(CyclopsHolographicHUD))]
+    internal class HolographicHudPatches
+    {
+        private static bool lastState = false;
+        /// <summary>
+        /// Patches the Player Awake method with prefix code.
+        /// </summary>
+        /// <param name="__instance"></param>
+        [HarmonyPatch(nameof(CyclopsHolographicHUD.Update))]
+        [HarmonyPrefix]
+        public static bool Update_Prefix(CyclopsHolographicHUD __instance, ref List<CyclopsHolographicHUD.DamageIcon> ___damageIcons, ref Transform ___subRootTransform)
+        {
+            LargeSubRepair largeSubRepair = ___subRootTransform.gameObject.GetComponent<LargeSubRepair>();
+            if (largeSubRepair == null)
+            {
+                return true;
+            }
+
+            if (largeSubRepair.repairBotActive == false && lastState == false)
+            {
+                // No point in updating this thing if there are no bots active.
+                // Hopefully it saves some performance.
+                return true;
+            }
+
+            lastState = largeSubRepair.repairBotActive;
+
+            if (___damageIcons == null)
+            {
+                RepairPlugin.Log.LogError("___damaeIcons is null!");
+                return true;
+            }
+
+            foreach (CyclopsHolographicHUD.DamageIcon damageIcon in ___damageIcons)
+            {
+                if (damageIcon != null)
+                {
+                    if (damageIcon.damageIcon != null && damageIcon.refDamage != null)
+                    {
+                        if (largeSubRepair.damageTarget == damageIcon.refDamage.GetComponent<CyclopsDamagePoint>() && largeSubRepair.botMode >= 3 && largeSubRepair.repairBotActive)
+                        {
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().warningPing.GetComponent<Image>().color = Color.green;
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().damageText.GetComponentInChildren<Image>().color = Color.green;
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().lineRenderer.startColor = Color.green;
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().lineRenderer.endColor = Color.green;
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().lineRenderer.gameObject.GetComponent<Image>().color = Color.green;
+                        }
+                        else
+                        {
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().warningPing.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().damageText.GetComponentInChildren<Image>().color = new Color(1, 1, 1, 1);
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().lineRenderer.startColor = new Color(1, 0.9098f, 0.1922f, 1);
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().lineRenderer.endColor = new Color(1, 0.9098f, 0.1922f, 1);
+                            damageIcon.damageIcon.GetComponent<CyclopsHolographicHUD_WarningPings>().lineRenderer.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                        }
+                    }
+                    else
+                    {
+                        RepairPlugin.Log.LogError("Could Not Find 'damageIcon' or 'refDamage' in damageIcon!");
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
